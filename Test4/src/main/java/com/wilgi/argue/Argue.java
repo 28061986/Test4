9728d449a6d6389dc8e1f3dd20bda75c5a614284
manetskiy <sergeymanetskiy@gmail.com>
2016-05-23 14:01:11 +0300

    second opinion is changed

--- Test4/src/main/java/com/wilgi/argue/Argue.java
+++ Test4/src/main/java/com/wilgi/argue/Argue.java
@@ -7,7 +7,7 @@ public class Argue {
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(2);
         service.submit(new ArgueTask("London is capital of Great Britain", 20));
-        service.submit(new ArgueTask("I don't care", 20));
+        service.submit(new ArgueTask("It's not true!!!", 20));
         service.shutdown();
     }
 }
