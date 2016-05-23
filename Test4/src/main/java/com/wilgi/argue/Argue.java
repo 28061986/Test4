516966327f40a8753ecb0dddcc9a43ecf6289f07
manetskiy <sergeymanetskiy@gmail.com>
2016-05-23 13:13:46 +0300

    Base commit

---
+++ Test4/src/main/java/com/wilgi/argue/Argue.java
@@ -0,0 +1,13 @@
+package com.wilgi.argue;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Argue {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        service.submit(new ArgueTask("London is capital of Great Britain", 20));
+        service.submit(new ArgueTask("I don't care", 20));
+        service.shutdown();
+    }
+}
---
+++ Test4/src/main/java/com/wilgi/argue/ArgueTask.java
@@ -0,0 +1,25 @@
+package com.wilgi.argue;
+
+public class ArgueTask implements Runnable {
+
+    private String opinion;
+    private int times;
+
+    public ArgueTask(String opinion, int times) {
+        this.opinion = opinion;
+        if (times > 0)
+            this.times = times;
+        else this.times = 10;
+    }
+
+    public void run() {
+        for (int i = 0; i < times; i++) {
+            System.out.println(opinion + " : " + (i + 1 ) + " time.");
+            try {
+                Thread.currentThread().sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
