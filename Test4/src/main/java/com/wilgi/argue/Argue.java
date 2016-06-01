b9e66e51a5ed88d30927364843c61ea0b69a6d4f
manetskiy <sergeymanetskiy@gmail.com>
2016-06-01 12:32:22 +0300

    Comment from master

--- Test4/src/main/java/com/wilgi/argue/Argue.java
+++ Test4/src/main/java/com/wilgi/argue/Argue.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 public class Argue {
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(2);
+		//comment from master
         service.submit(new ArgueTask("London is capital of Great Britain", 20));
         service.submit(new ArgueTask("It's not true!!!", 20));
         service.shutdown();
