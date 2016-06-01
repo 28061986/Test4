f4ac19f52f54de99aa8d4e5f8808afb506f18c6e
manetskiy <sergeymanetskiy@gmail.com>
2016-06-01 10:37:17 +0300

    4 test

--- Test4/src/main/java/com/wilgi/argue/Argue.java
+++ Test4/src/main/java/com/wilgi/argue/Argue.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Executors;
 public class Argue {
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(2);
-        service.submit(new ArgueTask("Errr2225555", 20));
+        service.submit(new ArgueTask("Errr222555", 20));
         service.submit(new ArgueTask("New222222222", 21));
         service.shutdown();
     }
